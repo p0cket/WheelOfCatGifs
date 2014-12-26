2c67c39569ddcf1c23d380a3b35aeef75ec4edd8
Pocket <p0ck31@gmail.com>
2014-12-26 14:21:19 -0800

    Make input functional

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express')
 var app = express()
 var catGifs = ["http://i427.photobucket.com/albums/pp355/NyackBosco/music/dj-cat.gif", "http://i465.photobucket.com/albums/rr14/themishkin/GIFs/party-fails-party-hard-weekend-cats-1.gif",];
+//bodyparser for sending the data
+var bodyParser = require('body-parser')
 
 app.use(express.static(__dirname));
+//bodyparser for sending the data
+app.use(bodyParser());
 app.get('/', function (req, res) {
   res.sendfile('index.html')
 })
@@ -14,7 +18,7 @@ app.get('/thecatgif', function(req,res) {
 	console.log(randomNum);
 	var newCatGif = catGifs[randomNum];
 	res.send(newCatGif);
-})
+});
 
 // app.get('/answer/:chosenAnswer', function(req, res) {
 // 	var chosenAnswer = req.params.chosenAnswer;
@@ -38,3 +42,14 @@ app.get('/thecatgif', function(req,res) {
   console.log('Example app listening at http://%s:%s', host, port)
 })
 
+app.post('/', function(req, res) {
+	var postParams = req.body;
+	var url = postParams.url;
+
+	// Add to db
+	// console.log(url);
+	console.log(url);
+	catGifs.push(url);
+	// res.render('index');
+	res.sendfile('index.html')
+});
